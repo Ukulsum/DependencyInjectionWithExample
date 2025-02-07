68e7054e85b16a3155de10d442b4548e2a3c34ae
ukulsum <ukulsum1280@gmail.com>
2025-02-08 00:01:36 +0600

    Add project files.

---
+++ DIExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+
+        //constructor
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
